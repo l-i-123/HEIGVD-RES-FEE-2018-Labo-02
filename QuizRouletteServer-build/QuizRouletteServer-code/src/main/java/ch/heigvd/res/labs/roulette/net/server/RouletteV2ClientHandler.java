6cfa4621ef68503904492614bf6bef0d68e2a5c6
Elie Kin <elie.ndjoli@outlook.com>
2018-04-04 15:16:08 +0200

    attention à la casse

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
@@ -1,9 +1,18 @@
 package ch.heigvd.res.labs.roulette.net.server;
 
+import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
 import ch.heigvd.res.labs.roulette.data.IStudentsStore;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
+import ch.heigvd.res.labs.roulette.data.Student;
+import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This class implements the Roulette protocol (version 2).
@@ -11,14 +20,76 @@ import java.io.OutputStream;
  * @author Olivier Liechti
  */
 public class RouletteV2ClientHandler implements IClientHandler {
+  final static Logger LOG = Logger.getLogger(RouletteV2ClientHandler.class.getName());
+
+  private final IStudentsStore store;
 
   public RouletteV2ClientHandler(IStudentsStore store) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of gen  
+    this.store = store;
+    //throw new UnsupportedOperationException("Not supported yet."); //To change body of gen
   }
 
   @Override
   public void handleClientConnection(InputStream is, OutputStream os) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    PrintWriter writer = new PrintWriter(new OutputStreamWriter(os));
+
+    writer.println("Hello. Online HELP is available. Will you find it?");
+    writer.flush();
+
+    String command;
+    boolean done = false;
+    while (!done && ((command = reader.readLine()) != null)) {
+      LOG.log(Level.INFO, "COMMAND: {0}", command);
+      switch (command.toUpperCase()) {
+        case RouletteV2Protocol.CMD_RANDOM:
+          RandomCommandResponse rcResponse = new RandomCommandResponse();
+          try {
+            rcResponse.setFullname(store.pickRandomStudent().getFullname());
+          } catch (EmptyStoreException ex) {
+            rcResponse.setError("There is no student, you cannot pick a random one");
+          }
+          writer.println(JsonObjectMapper.toJson(rcResponse));
+          writer.flush();
+          break;
+        case RouletteV2Protocol.CMD_HELP:
+          writer.println("Commands: " + Arrays.toString(RouletteV2Protocol.SUPPORTED_COMMANDS));
+          break;
+        case RouletteV2Protocol.CMD_INFO:
+          InfoCommandResponse response = new InfoCommandResponse(RouletteV2Protocol.VERSION, store.getNumberOfStudents());
+          writer.println(JsonObjectMapper.toJson(response));
+          writer.flush();
+          break;
+        case RouletteV2Protocol.CMD_LOAD:
+          writer.println(RouletteV2Protocol.RESPONSE_LOAD_START);
+          writer.flush();
+          store.importData(reader);
+          writer.println(RouletteV2Protocol.RESPONSE_LOAD_DONE);
+          writer.flush();
+          break;
+        case RouletteV2Protocol.CMD_CLEAR:
+          store.clear();
+          break;
+        case RouletteV2Protocol.CMD_LIST:
+          List<Student> studentList = store.listStudents();
+          if(studentList.size() == 0){
+            writer.println("There is no student");
+          }
+          else{
+            writer.println(JsonObjectMapper.toJson(studentList));
+            writer.flush();
+          }
+          break;
+        case RouletteV2Protocol.CMD_BYE:
+          done = true;
+          break;
+        default:
+          writer.println("Huh? please use HELP if you don't know what commands are available.");
+          writer.flush();
+          break;
+      }
+      writer.flush();
+    }
   }
 
 }
